d294c5dcd5dfb580fa1d028170849d16ca466364
Hope Ngerebara <hope.ngerebara@andela.com>
2017-10-25 19:09:40 +0100

    initial commit

---
+++ courses/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
