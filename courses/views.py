adc027ccfce9482648ad23923854a154dd6eb5f7
Hope Ngerebara <hope.ngerebara@andela.com>
2017-10-26 16:46:34 +0100

    blog first commit

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def course_list(request):
+    courses = Course.Objects.all()
+    output = ', '.join(courses)
+    return HttpResponse(output)
